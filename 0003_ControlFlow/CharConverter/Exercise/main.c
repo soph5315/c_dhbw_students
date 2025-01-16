612d54bfc9892c7101bc2ff5f58d9d00a4027269
SOPHIE DUERR <sophie.duerr@mercedes-benz.com>
2025-01-16 10:06:57 +0100

    my charConverter

--- 0003_ControlFlow/CharConverter/Exercise/main.c
+++ 0003_ControlFlow/CharConverter/Exercise/main.c
@@ -9,18 +9,66 @@ int main()
     scanf("%c", &character);
 
     // is numeric
+    bool isNumeric = false;
+    if ((character >= '0') && (character <= '9'))
+    {
+        isNumeric = true;
+        printf("\nis numeric: %d\n", isNumeric);
+    }
+                     //%d int, %c char (ASCII Code 1 :))
 
     // is upper case
+    bool isUpperCase = false;
+    char result1 = 45;
+    if ((character >= 'A') && (character <= 'Z'))
+    {
+        isUpperCase = true;
+        printf("\nis Upper: %d\n", isUpperCase);
 
+         // to lower case
+        if (isUpperCase)
+        {
+            char result1 = character + 32;
+            printf("\nis now lower case: %c\n", result1);
+        }
+    }
+    
     // is lower case
+    bool isLowerCase = false;
+    char result2 = 45;
+    if ((character >= 'a') && (character <= 'z'))
+    {
+        isLowerCase = true;
+        printf("\nis lower: %d\n", isLowerCase);
 
+        // to upper case
+        if (isLowerCase)
+        {
+            char result2 = character - 32;
+            printf("\nis now Upper case: %c\n", result2);
+        }
+    }
+    
     // is alpha
-
+    bool isAlpha = false;
+    if (isLowerCase || isUpperCase)
+    {
+        isAlpha = true;
+        printf("\nis alpha: %d\n", isAlpha);
+    }
+    
     // is alpha numeric
+    bool isAlphaNumeric;
+    if (isAlpha || isNumeric)
+    {
+        isAlphaNumeric = true;
+        printf("\nis alpha numeric: %d\n", isAlphaNumeric);
+    }
 
-    // to upper case
-
-    // to lower case
+    else
+    {
+        printf("\nI don't know what that is %c\n", 1);
+    }
 
     return 0;
 }
